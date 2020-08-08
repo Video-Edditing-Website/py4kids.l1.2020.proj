7c7c7bf749bca21bd8957d75cf7765eb35e95857
Abhijat Vatsyayan <abhijat.vatsyayan@gmail.com>
2020-08-08 14:40:01 -0400

    Committing template code

--- server.py
+++ server.py
@@ -1,10 +1,19 @@
 from flask import Flask
+import  flask as f
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
+
+@app.route("/welcome/<email>")
+def do_welcome(email):
+    print("Email supplied:{}".format(email))
+
+    file_contents = f.render_template("welcome.html", user_name=email)
+    return file_contents
 
 if __name__ == '__main__':
     app.run(host='localhost', port=8181)
