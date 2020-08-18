c755cb672984dda667bf6d40cb988ad3e909d7b7
Abhijat Vatsyayan <abhijat.vatsyayan@gmail.com>
2020-08-18 19:12:18 -0400

    Work in progress review

--- server.py
+++ server.py
@@ -1,8 +1,19 @@
 from flask import Flask, request, redirect
 import flask as f
 import json
+import uuid
+import os
 
 app = Flask(__name__)
+
+
+def save_review(review_id, review_to_save):
+    file_name = os.path.join("reviews", review_id+".json")
+    f = open(file_name, "w")
+    json.dump(review_to_save, f)
+    f.close() 
+    
+
 def read_users_dict():
     flask = open("users.json", "r")
     users = json.load(flask)
@@ -18,7 +29,7 @@ def save_users_dict(users):
 
 @app.route("/")
 def show_login_page():
-    return  redirect("/static/login.html")
+    return redirect("/static/login.html")
 
 
 @app.route("/login", methods=["post"])
@@ -32,6 +43,27 @@ def do_login():
 
     return f.render_template("login_failed.html")
 
+
+@app.route("/submit_review", methods=["post"])
+def do_submit_review():
+    game = request.form['games']
+    title = request.form['title']
+    stars = request.form['stars']
+    stars = int(stars)
+    review = request.form['review']
+    print("[.do_submit_review()] game = {}".format(game))
+    print("[.do_submit_review()] title, stars = {}, {}".format(title, stars))
+    print("[.do_submit_review()] review text = {}".format(review))
+    review_id = uuid.uuid1()
+    review_id = str(review_id)
+    review_to_save = {'game':game, 'title':title, 'stars':stars, 'review':review}
+    save_review(review_id, review_to_save)
+    return f.render_template("thanks_for_review.html") 
+
+
+@app.route("/game_review")
+def do_game_review():
+    return f.render_template("game_review.html")
 
 @app.route("/sign_up", methods=["post"])
 def do_sign_up():
@@ -50,4 +82,4 @@ def do_sign_up():
 
 
 if __name__ == '__main__':
-   app.run(host='localhost', port=8181)
+   app.run(host='localhost', port=8181, debug=True)
